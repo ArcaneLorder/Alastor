5a7af9c7137710f0a202fbcf9539e074e3d3a1b8
root <root@cute-box-1.localdomain>
2024-07-10 05:59:41 -0400

    adjust files struct

--- src/common/Sql.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once 
-
-#include "Common.h"
-
-#include "Thirdparty/MySQL Connector C++ 8.3/include/mysql/jdbc.h"
-#pragma comment(lib, "Thirdparty/MySQL Connector C++ 8.3/lib64/vs14/mysqlcppconn8-static.lib")
-
-
-
--- src/common/Thirdparty/lua/lua.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "util/json.h"
-
-newobj::json script_exec(const nstring& script,const nstring& function,const newobj::json& param,nstring& error_msg);
